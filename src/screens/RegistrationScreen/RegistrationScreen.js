8d7d341bd01af36e4a5e09e118264393e752ee10
Obinna Mokwe <obinnamokwe@Obinnas-MacBook-Air.local>
2021-02-18 10:34:23 -0600

    registrationComp committed

--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
+import { firebase } from '../../firebase/config';
+
+
 
 export default function RegistrationScreen({ navigation }) {
 	const [fullName, setFullName] = useState('');
@@ -9,7 +12,39 @@ export default function RegistrationScreen({ navigation }) {
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
 
-	const onRegisterPress = () => {};
+	const onRegisterPress = () => {
+        if (password !== confirmPassword) {
+            alert("Passwords don't match")
+            return
+        }
+
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(response => {
+                const uid = response.user.uid
+                const data = {
+                    id: uid,
+                        email,
+                        fullName,
+                };
+                const usersRef = firebase.firestore().collection('users')
+                usersRef
+                    .doc(uid)
+                    .set(data)
+                    .then(() => {
+                      navigation.navigate('Home', {user: data})  
+                    })
+                    .catch(error => {
+                        alert(error)
+                    });
+
+            })
+            .catch(error => {
+                alert(error)
+            });
+
+    }
 
 	const backToLogin = () => {
 		navigation.navigate('Login');
